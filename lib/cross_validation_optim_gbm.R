40e15f8937145a9a709ba1d825e9d632151cd6f4
Alex DeKinder <ad3540@columbia.edu>
2019-10-27 15:35:03 -0400

    Create cross_validation_optim_gbm.R

---
+++ lib/cross_validation_optim_gbm.R
@@ -0,0 +1,26 @@
+########################
+### Cross Validation ###
+########################
+
+### Author: Chengliang Tang
+### Project 3
+
+cv.function <- function(dat_train,K,n){
+  ### Input:
+  ### - train data frame
+  ### - K: a number stands for K-fold CV
+  ### - s: shrinkage
+  
+  
+    gbm_model <- gbm(formula = emotion_idx ~.,
+                     distribution = "gaussian",
+                     data = dat_train,
+                     cv.folds = K,
+                     n.trees = n)
+
+    
+    error <- c(mean(gbm_model$train.error),sd(gbm_model$train.error)) #training error
+    print(error)
+
+  return(error)
+}
